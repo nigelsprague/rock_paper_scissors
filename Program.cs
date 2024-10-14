3fda671a9dbd3ca3f793827fad68982ea6c9f91f
Nigel Sprague <spraguenigel27@gmail.com>
2024-10-14 15:41:39 -0600

    init commit, user input and computer input

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Dynamic;
+
+internal class Program
+{
+  private static void Main()
+  {
+    Console.WriteLine("Rock, Paper, Scissors");
+    string userHand = ChooseHand();
+    string computerHand = GetComputerHand();
+  }
+
+  static string ChooseHand()
+  {
+    Console.Clear();
+    Console.WriteLine("Choose A Hand");
+    Console.WriteLine("1. Rock");
+    Console.WriteLine("2. Paper");
+    Console.WriteLine("3. Scissors");
+
+    string? userInput = Console.ReadLine();
+    if (userInput != "1")
+    {
+      if (userInput != "2")
+      {
+        if (userInput != "3")
+        {
+          throw new Exception("Not a valid input, try again.");
+        }
+      }
+    }
+    if (userInput == "1")
+    {
+      Console.WriteLine("Rock");
+    }
+    if (userInput == "2")
+    {
+      Console.WriteLine("Paper");
+    }
+    if (userInput == "3")
+    {
+      Console.WriteLine("Scissors");
+    }
+    return "";
+  }
+  static string? GetComputerHand()
+  {
+    int randomNumber = new Random().Next(1, 4);
+    if (randomNumber == 1)
+    {
+      Console.WriteLine("Rock");
+    }
+    if (randomNumber == 2)
+    {
+      Console.WriteLine("Paper");
+    }
+    if (randomNumber == 3)
+    {
+      Console.WriteLine("Scissors");
+    }
+    return "";
+  }
+}
